3536969dfcd6bfb5c4e9b3486c2146d6e2f470c8
bhagatameya <ameya.b@netizens.email>
2023-08-02 07:36:56 +0530

    final

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import bodyParser from 'body-parser'
+import compression from 'compression'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { config } from 'dotenv'
+import express from 'express'
+import http from 'http'
+import mongoose from 'mongoose'
+import router from './router'
+config();
+
+const app = express();
+
+app.use(cors({
+  credentials:true,
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
+
+server.listen(8080,()=>{
+  console.log("Server Running on http://localhost:8080/")
+})
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.DB_URL).then(()=>{
+  console.log("DB Connected");
+});
+mongoose.connection.on('error',(error: Error)=>{console.log(error)
+});
+
+app.use('/',router())
